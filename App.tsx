fab1fcf53cbb159556ed0cd792fa53ae3e607ad3
califnena <realpropertywerx@gmail.com>
2025-11-16 14:20:46 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,53 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n/config";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ResetPassword from "./pages/ResetPassword";
+import PlannerApp from "./pages/PlannerApp";
+import Profile from "./pages/Profile";
+import Subscription from "./pages/Subscription";
+import AboutUs from "./pages/AboutUs";
+import LegalForms from "./pages/LegalForms";
+import CoachAssistant from "./pages/CoachAssistant";
+import NextSteps from "./pages/NextSteps";
+import CaseDetail from "./pages/CaseDetail";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <I18nextProvider i18n={i18n}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/legal-forms" element={<LegalForms />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/app" element={<PlannerApp />} />
+            <Route path="/app/profile" element={<Profile />} />
+            <Route path="/app/profile/subscription" element={<Subscription />} />
+            <Route path="/vip-coach" element={<CoachAssistant />} />
+            <Route path="/next-steps" element={<NextSteps />} />
+            <Route path="/next-steps/case/:caseId" element={<CaseDetail />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </I18nextProvider>
+  </QueryClientProvider>
+);
+
+export default App;
